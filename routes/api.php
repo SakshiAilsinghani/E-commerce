b7d810dc2f230defec16f710ed65966e2093a85d
ailsakshi2001@gmail.com <ailsakshi2001@gmail.com>
2022-11-08 23:31:30 +0530

    made controller, route and logic to find sellers associated with a category

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,4 @@ Route::resource('buyers.products', \App\Http\Controllers\Buyer\BuyerProductsCont
 Route::resource('buyers.sellers', \App\Http\Controllers\Buyer\BuyerSellersController::class)->only(['index']);
 Route::resource('buyers.categories', \App\Http\Controllers\Buyer\BuyerCategoriesController::class)->only(['index']);
 Route::resource('categories.products', \App\Http\Controllers\Category\CategoryProductsController::class)->only(['index']);
+Route::resource('categories.sellers', \App\Http\Controllers\Category\CategorySellersController::class)->only(['index']);
