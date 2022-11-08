cc745f1cbb7c435f81371a80b38931910fdb6a55
ailsakshi2001@gmail.com <ailsakshi2001@gmail.com>
2022-11-08 14:18:53 +0530

    completed categories opertaion nut with errors

---
+++ app/Http/Controllers/Category/CategoriesController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Http\Controllers\ApiController;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class CategoriesController extends  ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() :JsonResponse
+    {
+        $categories= Category::all();
+        dd($categories);
+        return $this->showAll($categories);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => 'required|min:2|max:255',
+            'description' => 'required|min:2'
+        ];
+        $this->validate($request, $rules);
+
+        $category = Category::create($request->only(['name', 'description']));
+        return $this->showOne($category, 201);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return $this->showOne($category);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $category->fill($request->only(['name', 'description']));
+        if($category->isClean()) {
+            return $this->errorResponse('You need to update some field!', 422);
+        }
+
+        $category->save();
+        return $this->showOne($category);
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return $this->showOne($category, 204);
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+Route::resource('categories', \App\Http\Controllers\Category\CategoriesController::class);
 Route::resource('buyers',\App\Http\Controllers\Buyer\BuyersController::class)->only(['index','show']);
 Route::resource('users', \App\Http\Controllers\User\UsersController::class);
 Route::resource('sellers', \App\Http\Controllers\Seller\SellersController::class)->only(['index', 'show']);
