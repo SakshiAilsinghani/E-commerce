c2ce7f99b5e7e4f7998c31c2d297a237626652df
ailsakshi2001@gmail.com <ailsakshi2001@gmail.com>
2022-11-08 13:26:33 +0530

    added global seller scope and used it

--- app/Models/Seller.php
+++ app/Models/Seller.php
@@ -3,12 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Scopes\SellerScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Seller extends User
 {
     use HasFactory;
     protected $table = "users";
+
+    protected static function boot()
+    {
+        parent::boot();
+        self::addGlobalScope(new SellerScope());
+    }
+
 
     public function products()
     {
