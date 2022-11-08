7de722fe14ea76655d5cee87e1d7e9e71aea7b3f
ailsakshi2001@gmail.com <ailsakshi2001@gmail.com>
2022-11-09 00:02:51 +0530

    added controller, routes and logic to handle seller products operations

---
+++ app/Http/Controllers/Seller/SellerProductsController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Seller;
+
+use App\Http\Controllers\ApiController;
+use App\Models\Product;
+use App\Models\Seller;
+use App\Models\User;
+use App\Traits\ApiResponser;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class SellerProductsController extends ApiController
+{
+    public function index(Seller $seller): JsonResponse
+    {
+        $products = $seller->products;
+        return $this->showAll($products);
+    }
+
+    public function store(Request $request, User $seller): JsonResponse
+    {
+        $rules = [
+            'name' => 'required|min:3',
+            'description' => 'required|min:5',
+            'quantity' => 'required|integer|min:1',
+            'image' => 'required|image'
+        ];
+
+        $this->validate($request, $rules);
+        $data = $request->all();
+        $data['status'] = Product::UNAVAILABLE_PRODUCT;
+        $data['image'] = '1.jpg'; //TODO: We need to change this!
+        $data['seller_id'] = $seller->id;
+
+        $product = Product::create($data);
+        return $this->showOne($product, 201);
+    }
+
+    public function update(Request $request, Seller $seller, Product $product): JsonResponse
+    {
+        // TODO: Validate the $seller is same as requesting user
+        $rules = [
+            'quantity' => 'integer|min:1',
+            'status' => 'in:' . Product::UNAVAILABLE_PRODUCT . ',' . Product::AVAILABLE_PRODUCT,
+            'image' => 'image'
+        ];
+
+        $this->validate($request, $rules);
+
+        $this->verifySeller($seller, $product);
+
+        $product->fill(
+            $request->only([
+                'name',
+                'description',
+                'quantity'
+            ])
+        );
+
+        if($request->has('status')) {
+            $product->status = (int)$request->status;
+
+            if($product->isAvailable() && $product->categories()->count() === 0) {
+                throw new HttpException(409, "A product must belong to atleast one category to be available!");
+            }
+        }
+
+        // TODO: Update image if the image was passed.
+
+        if($product->isClean()) {
+            throw new HttpException(422,"You have not updated anything");
+        }
+
+        $product->save();
+        return $this->showOne($product);
+    }
+
+    public function destroy(Seller $seller, Product $product)
+    {
+        $this->verifySeller($seller, $product);
+        $product->delete();
+        return $this->showOne($product, 204);
+    }
+
+    private function verifySeller(Seller $seller, Product $product)
+    {
+        if($seller->id !== $product->seller_id) {
+            throw new HttpException(422, "You are trying to update someone else's product");
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::resource('categories.buyers', \App\Http\Controllers\Category\CategoryBuye
 
 Route::resource('sellers.categories', \App\Http\Controllers\Seller\SellerCategoriesController::class)->only(['index']);
 Route::resource('sellers.buyers', \App\Http\Controllers\Seller\SellerBuyersController::class)->only(['index']);
+Route::resource('sellers.products', \App\Http\Controllers\Seller\SellerProductsController::class)->except(['create', 'edit', 'show']);
