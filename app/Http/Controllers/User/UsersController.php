e727f05916f9e3654a4b35eff93ca3158c5ea05d
ailsakshi2001@gmail.com <ailsakshi2001@gmail.com>
2022-11-09 16:34:31 +0530

    added verification code

--- app/Http/Controllers/User/UsersController.php
+++ app/Http/Controllers/User/UsersController.php
@@ -154,4 +154,17 @@ class UsersController extends ApiController
         return $this->showOne($user, 204);
 
     }
+
+    public function verify($token)
+    {
+        $user = User::where('verification_token', $token)->firstOrFail();
+
+        $user->verified = User::VERIFIED_USER;
+        $user->verification_token = null;
+
+        $user->save();
+
+        return $this->showMessage("The account has been verified!");
+    }
+
 }
