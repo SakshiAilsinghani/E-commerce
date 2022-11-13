e55ef92aa4ae12fd1472c2d099fb8309419902fd
ailsakshi2001@gmail.com <ailsakshi2001@gmail.com>
2022-11-13 17:44:44 +0530

    added new middleware for converting input keys to database keys

---
+++ app/Http/Middleware/TransformInput.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class TransformInput
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, $transformer)
+    {
+        $transformedInput = [];
+        foreach ($request->request->all() as $input => $value) {
+            $transformedInput[$transformer::attributeMapper($input)] = $value;
+        }
+        $request->replace($transformedInput);
+        return $next($request);
+    }
+}
